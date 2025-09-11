684d7a1cea16053a103de2c0fcab2e13d5dde9b0
DeveloperManikDas <developermanikdas@gmail.com>
2025-09-11 10:45:47 +0530

    Is Prime

---
+++ 100 Days of DSA Challange/Day 5/IsPrime.java
@@ -0,0 +1,18 @@
+public class IsPrime {
+
+    public static boolean checkPrime(int n) {
+        if(n < 2){
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(checkPrime(2));
+    }
+}
