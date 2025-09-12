bde8d43a2cd0f800a19dab14c426bf138e91f037
DeveloperManikDas <developermanikdas@gmail.com>
2025-09-12 08:50:16 +0530

    initial commit

---
+++ 100 Days of DSA Challange/Day 6/Function1.java
@@ -0,0 +1,9 @@
+public class Function1 {
+    public static int avg(int a, int b, int c){
+        return (a+b+c)/3;
+    }
+
+    public static void main(){
+        System.out.println(avg(3,5,7));
+    }
+}
