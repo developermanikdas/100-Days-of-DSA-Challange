439b7dce4c3dfb117a184656fb33e0bfae76dd6e
DeveloperManikDas <developermanikdas@gmail.com>
2025-09-13 15:59:14 +0530

    invertedHalfPyramidsNumbers

---
+++ 100 Days of DSA Challange/Day 7/invertedHalfPyramidsNumbers.java
@@ -0,0 +1,15 @@
+public class invertedHalfPyramidsNumbers {
+
+    public static void printInvertedHalfPyramidWithNumbers(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main() {
+        printInvertedHalfPyramidWithNumbers(5);
+    }
+}
